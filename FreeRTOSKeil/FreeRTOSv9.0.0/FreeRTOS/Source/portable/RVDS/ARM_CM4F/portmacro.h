82bf27b6cf4c2cdab289f3855b4666bdfa7b0c72
Robson Rojas <robson.andrade@iatecam.org.br>
2017-05-18 13:01:15 -0400

    Adding specific project source to the freertoskeil project

--- FreeRTOSKeil/FreeRTOSv9.0.0/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+++ FreeRTOSKeil/FreeRTOSv9.0.0/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
@@ -165,11 +165,11 @@ extern void vPortExitCritical( void );
 	#define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
 #endif
 
-#if configUSE_PORT_OPTIMISED_TASK_SELECTION == 1
+#if (configUSE_PORT_OPTIMISED_TASK_SELECTION == 1)
 
 	/* Check the configuration. */
-	#if( configMAX_PRIORITIES > 32 )
-		#error configUSE_PORT_OPTIMISED_TASK_SELECTION can only be set to 1 when configMAX_PRIORITIES is less than or equal to 32.  It is very rare that a system requires more than 10 to 15 difference priorities as tasks that share a priority will time slice.
+	#if ( configMAX_PRIORITIES > 32 )
+			#error configUSE_PORT_OPTIMISED_TASK_SELECTION can only be set to 1 when configMAX_PRIORITIES is less than or equal to 32.  It is very rare that a system requires more than 10 to 15 difference priorities as tasks that share a priority will time slice.
 	#endif
 
 	/* Store/clear the ready priorities in a bit map. */
